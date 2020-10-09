15cf7f065093be245dcc9b75ae1cec47c0f04fa0
artistotless <34582815+artistotless@users.noreply.github.com>
2020-10-09 20:00:52 +0300

    added_aim_ik
    ф

---
+++ Assets/ShootSystem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootSystem : MonoBehaviour
+{
+    public float sensitivity = 2f;
+    public Transform aim;
+
+    Vector3 direction;
+    Vector3 newPosition;
+
+    private void Start()
+    {
+        newPosition = Vector3.zero;
+        direction = new Vector3(aim.position.x, aim.position.y, aim.position.z);
+        var cameraPose = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        aim.position = new Vector3(cameraPose.x, cameraPose.y, 0.0f);
+        Cursor.visible = false;
+       
+       
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxis("Mouse X");
+        direction.y = Input.GetAxis("Mouse Y");
+        newPosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        newPosition.y = Camera.main.ScreenToWorldPoint(Input.mousePosition).y;
+
+        aim.position = newPosition ;
+        // aim.position += direction * Time.deltaTime * sensitivity;
+    }
+}
