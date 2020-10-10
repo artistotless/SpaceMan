b74dac03bd295c3752cac27cb8f10b77c3978af5
artistotless <34582815+artistotless@users.noreply.github.com>
2020-10-10 15:20:31 +0300

    v1.0.4

--- Assets/ShootSystem.cs
+++ Assets/ShootSystem.cs
@@ -15,6 +15,9 @@ public class ShootSystem : MonoBehaviour
     Vector3 newPosition;
     IKPicker ikpicker;
 
+    Rigidbody2D recoilArm;
+    Transform aimDirectionArm;
+
     private void Start()
     {
         ikpicker = gameObject.GetComponent<IKPicker>();
@@ -25,13 +28,27 @@ public class ShootSystem : MonoBehaviour
         aim.position = new Vector3(cameraPose.x, cameraPose.y, 0.0f);
         Cursor.visible = false;
        
-       
+        recoilArm = GameObject.FindGameObjectWithTag("RecoilArm").GetComponent<Rigidbody2D>();
+        aimDirectionArm = recoilArm.transform.GetChild(0);
 
+    }
 
+    void Shoot()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 recoilDirection = Vector3.zero;
+            recoilDirection.y = currentWeapon.recoil  * (aimDirectionArm.transform.rotation.normalized.z < 0 ? 1 : -1);
+            recoilDirection.x = -currentWeapon.recoil;
+            //Debug.Log("Pressed Fire!");
+            recoilArm.AddForce(recoilDirection, ForceMode2D.Impulse);
+            Debug.Log(recoilDirection);
+        }
     }
     // Update is called once per frame
     void Update()
     {
+        Shoot();
         direction.x = Input.GetAxis("Mouse X");
         direction.y = Input.GetAxis("Mouse Y");
         newPosition.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
